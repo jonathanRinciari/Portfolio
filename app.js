6abb48da0fcf4b0562a22ab7e2695417e3ea3fa5
Jon Rinciari <jrince@Jons-MacBook-Pro.local>
2017-08-30 17:11:14 -0400

    Added landing page

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require("express"),
 	app = express();
 
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", function(req, res){
-	res.send("This is the Loading Page!");
+	res.render("LandingPage")
 });
 
 app.get("/homepage", function(req, res){
-	res.send("This is the home page");
+	res.render("homepage")
 });
 
 

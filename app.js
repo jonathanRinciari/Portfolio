c6ed13365015115d163afcbd859396a40ae3c737
Jon Rinciari <jrince@Jons-MacBook-Pro.local>
2017-09-07 16:23:44 -0400

    set up blog route

--- app.js
+++ app.js
@@ -87,6 +87,10 @@ app.get("/portfolio", function(req, res){
 	res.render("portfolio");
 });
 
+app.get("/blog", function(req, res){
+	res.render("blog");
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })

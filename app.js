35a0dbf4eba5b9ba08b321287b45b070de1c2633
Jon Rinciari <jrince@Jons-MacBook-Pro.local>
2017-09-12 22:35:18 -0400

    change error

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var express = require("express"),
 	app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASEURL, {useMongoClient: true}))
+mongoose.connect(process.env.DATABASEURL);
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
